a68a2324c06f7f9df40dfaa72473f9adc5706ca0
Kyoungwon Kang <73323028+kkwondev@users.noreply.github.com>
2023-01-26 17:37:09 +0900

    project 레이아웃 구상

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,15 +1,26 @@
 <template>
-  <p>PROJECT 페이지</p>
+  <div class="py-24 flex bor">
+    <div>
+      <img
+        src="http://hanametal.co.kr/v3/assets/img/common/main_logo.png"
+        alt="logo"
+      />
+    </div>
+    <div>
+      <h3>하나금속 <span>퍼블리싱</span></h3>
+      <p>디자이너에게 디자인을 받아 html css 로 퍼블리싱을 진행하였습니다.</p>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
-import { useRouter, useRoute } from "vue-router";
-const router = useRouter();
-const route = useRoute();
+// import { onMounted } from "vue";
+// import { useRouter, useRoute } from "vue-router";
+// const router = useRouter();
+// const route = useRoute();
 
-onMounted(() => {
-  alert(`${route.name?.toString()} 페이지는 준비중입니다.`);
-  router.push("/");
-});
+// onMounted(() => {
+//   alert(`${route.name?.toString()} 페이지는 준비중입니다.`);
+//   router.push("/");
+// });
 </script>
