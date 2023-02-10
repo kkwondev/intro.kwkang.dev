30852924ccdd6d956c13fb21e98c9fca95cc8562
Kyoungwon Kang <73323028+kkwondev@users.noreply.github.com>
2023-02-10 17:42:34 +0900

    css modify

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="w-full h-20 font-[Quicksand] bg-white sticky top-0">
+  <div
+    class="w-full h-20 font-[Quicksand] bg-white sticky top-0 tablet:h-auto tablet:border-[1px]"
+  >
     <nav
-      class="max-w-[1200px] mx-auto h-full flex justify-between items-center tablet:block tablet:p-2 tablet:text-center px-2"
+      class="max-w-[1200px] mx-auto h-full flex justify-between items-center tablet:block tablet:p-2 tablet:pb-0 tablet:text-center px-2"
     >
       <h1 class="text-lg font-semibold -tracking-[1.3px] text-slate-900">
         <router-link to="/"> &lt;kwkang.dev/&gt; </router-link>
       </h1>
-      <ul class="flex items-center h-full">
+      <ul class="flex items-center h-full py-4">
         <li
-          class="px-10 text-sm -tracking-wide tablet:block tablet:w-[33%] tablet:text-center"
+          class="text-sm -tracking-wide tablet:block tablet:w-[33%] tablet:text-center px-4"
         >
           <!-- <router-link
             to="https://unruly-algebra-5f4.notion.site/2e211b9df81f473ca887d06afccaef1b"
@@ -22,13 +24,13 @@
           <p class="cursor-pointer" @click="aboutToNotion">About</p>
         </li>
         <li
-          class="px-10 text-sm -tracking-wide tablet:block tablet:w-[33%] tablet:text-center"
+          class="text-sm -tracking-wide tablet:block tablet:w-[33%] tablet:text-center px-4"
         >
           <!-- <router-link to="/post">Post</router-link> -->
           <p class="cursor-pointer" @click="aboutToBlog">Post</p>
         </li>
         <li
-          class="px-10 text-sm -tracking-wide tablet:block tablet:w-[33%] tablet:text-center"
+          class="text-sm -tracking-wide tablet:block tablet:w-[33%] tablet:text-center px-4"
           :class="
             route.name === 'project'
               ? 'font-semibold opacity-100'
